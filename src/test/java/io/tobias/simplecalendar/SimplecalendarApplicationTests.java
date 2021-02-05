dc0bf0dc2a787709f8211d705e6deea15f66ca14
Tobias Hellmund <tobias.hellmund@iosb.fraunhofer.de>
2021-02-05 21:27:21 +0100

    project setup commit

---
+++ src/test/java/io/tobias/simplecalendar/SimplecalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tobias.simplecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
