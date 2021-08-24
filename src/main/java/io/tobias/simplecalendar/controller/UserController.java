fafacfdd58bd98e5389ac061bd92320d30d35299
T-Hellmund <hellmund.tobias@web.de>
2021-08-24 22:52:53 +0200

    elaborate user status controller and service

--- src/main/java/io/tobias/simplecalendar/controller/UserController.java
+++ src/main/java/io/tobias/simplecalendar/controller/UserController.java
@@ -6,12 +6,9 @@ import io.tobias.simplecalendar.repositories.CalendarUserRepository;
 import io.tobias.simplecalendar.service.UserService;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,9 +23,11 @@ import java.util.Optional;
 @RequestMapping("api/users")
 public class UserController {
 
+    UserService userService = new UserService();
 
     @Autowired
     CalendarUserRepository calendarUserRepository;
+
 
     @GetMapping("{id}")
     ResponseEntity<CalendarUser> getUser(@PathVariable Integer id) {
@@ -42,23 +41,17 @@ public class UserController {
         throw new IllegalArgumentException("User with id " + id + " not found.");
     }
 
+
     @GetMapping("/status")
     JsonObject getUserStatus() {
-        JsonObject response = new JsonObject();
-        final Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails) {
-            String username = ((UserDetails)principal).getUsername();
-            response.add("name", JsonParser.parseString(username));
-        } else {
-            String username = principal.toString();
-            response.add("name", JsonParser.parseString(username));
-        }
+        JsonObject response = UserService.retrieveUserStatus();
 
         return response;
     }
 
+
     @PostMapping
-    ResponseEntity<String> createUser (@RequestBody UserDTO userDto) {
+    ResponseEntity<String> createUser(@RequestBody UserDTO userDto) {
         UserService userService = new UserService();
         final CalendarUser calendarUser = userService.registerNewAccount(userDto);
         return new ResponseEntity<String>("Successfully created", HttpStatus.OK);
--- src/main/java/io/tobias/simplecalendar/service/UserService.java
+++ src/main/java/io/tobias/simplecalendar/service/UserService.java
@@ -3,7 +3,11 @@ import io.tobias.simplecalendar.dtos.UserDTO;
 import io.tobias.simplecalendar.model.CalendarUser;
 import io.tobias.simplecalendar.repositories.CalendarUserRepository;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
@@ -12,6 +16,29 @@ public class UserService {
     @Autowired
     CalendarUserRepository calendarUserRepository;
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+
+    public static JsonObject retrieveUserStatus() {
+        JsonObject userStatus = new JsonObject();
+        final Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        final Object isAuthenticated = SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
+        final Object authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails) principal).getUsername();
+            userStatus.add("name", JsonParser.parseString(username));
+            userStatus.add("isAuthenticated", JsonParser.parseString(isAuthenticated.toString()));
+            userStatus.add("authorities", JsonParser.parseString(authorities.toString()));
+
+        }
+        else {
+            String username = principal.toString();
+            userStatus.add("name", JsonParser.parseString(username));
+            userStatus.add("isAuthenticated", JsonParser.parseString(isAuthenticated.toString()));
+            userStatus.add("authorities", JsonParser.parseString(authorities.toString()));
+        }
+        return userStatus;
+    }
 
 
     public CalendarUser registerNewAccount(UserDTO userdto) {
