c467ee05d1f5de43a4b2cb438c72904c3ebc25d9
T-Hellmund <hellmund.tobias@web.de>
2021-08-26 08:00:13 +0200

    remove empty test

--- src/test/java/io/tobias/simplecalendar/SimplecalendarApplicationTests.java
+++ src/test/java/io/tobias/simplecalendar/SimplecalendarApplicationTests.java
@@ -1,13 +1,10 @@
 package io.tobias.simplecalendar;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SimplecalendarApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
